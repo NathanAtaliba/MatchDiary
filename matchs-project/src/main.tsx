b95508ae9f9d45d72c6389c99cda015e831a1fc6
NathanAtaliba <nathanataliba28@gmail.com>
2024-06-25 15:05:39 -0300

    fixed errors

--- matchs-project/src/main.tsx
+++ matchs-project/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import FirstPage from './pages/FirstPage.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <FirstPage />
   </React.StrictMode>,
 )
---
+++ matchs-project/src/pages/FirstPage.tsx
@@ -0,0 +1,11 @@
+function FirstPage() {
+  return (
+    <>
+      <div className='bg-slate-800 w-full h-content'>
+        123
+      </div>
+    </>
+  )
+}
+
+export default FirstPage
